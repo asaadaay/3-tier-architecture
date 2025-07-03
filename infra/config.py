76101473ef6174e63e720312b979c7da0546d3fe
asad <asad47195@gmail.com>
2025-07-03 19:38:24 -0300

    fetch secret values from .env

--- infra/config.py
+++ infra/config.py
@@ -1,7 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+git_pat_secret_arn = os.getenv("GIT_PAT_SECRET_ARN")
+git_repo_owner = os.getenv("GIT_REPO_OWNER")
+git_repo = os.getenv("GIT_REPO")
+git_branch = os.getenv("GIT_BRANCH")
+ecr_image_uri = os.getenv("ECR_IMAGE_URI")
+
 vpc_config = {
     "cidr": "10.0.0.0/20"
 }
-
 
 subnets_config = [
     {"name": "public", "type": "PUBLIC"},
@@ -11,13 +21,22 @@ subnets_config = [
 
 rds_config = {
     "allocated-storage": 100,
-    "instance-type": "db.t4g.micro",
+    "instance-type": "t4g.micro",
     "delete-automated-backups": True
 }
 
 ecs_task_def_config = {
     "cpu": 256,
     "memory_limit_mib": 512,
-    "container-port": 80,
-    "image-registry": "public.ecr.aws/docker/library/httpd:latest"
+    "container-port": 3000,
 }
+
+pipeline_config = {
+    "repo-owner": git_repo_owner,
+    "git-pat-secret-arn": git_pat_secret_arn,
+    "repo": git_repo,
+    "branch": git_branch,
+    "ecr-image-uri": ecr_image_uri
+}
+
+
